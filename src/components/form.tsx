cf5b0fa3149217d6b2590fd43e4c041938cc33e6
Benjamin Mehl <Benni.mehlBM@gmail.com>
2024-07-04 14:29:43 +0200

    sst next.js start

---
+++ src/components/form.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { FormEvent } from "react";
+import styles from "./form.module.css";
+
+export default function Form({ url }: { url: string }) {
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const file = (e.target as HTMLFormElement).file.files?.[0]!;
+
+        const image = await fetch(url, {
+            body: file,
+            method: "PUT",
+            headers: {
+                "Content-Type": file.type,
+                "Content-Disposition": `attachment; filename="${file.name}"`,
+            },
+        });
+
+        window.location.href = image.url.split("?")[0];
+    }
+
+    return (
+        <form
+            className={styles.form}
+            onSubmit={async (e) => {
+                e.preventDefault();
+
+                const file = (e.target as HTMLFormElement).file.files?.[0]!;
+
+                const image = await fetch(url, {
+                    body: file,
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": file.type,
+                        "Content-Disposition": `attachment; filename="${file.name}"`,
+                    },
+                });
+
+                window.location.href = image.url.split("?")[0];
+            }}
+        >
+            <input name="file" type="file" accept="image/png, image/jpeg" />
+            <button type="submit">Upload</button>
+        </form>
+    );
+}
