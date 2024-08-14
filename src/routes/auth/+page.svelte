975f39da2c549492c14eee6c9fb1ee52627138ad
Max Knerrich <max.knerrich@outlook.com>
2024-08-14 22:34:54 +0200

    updated login screen

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -2,4 +2,7 @@
 	import { signIn } from '@auth/sveltekit/client';
 </script>
 
-<button on:click={() => signIn('google')}>Google Login</button>
+<section class="container">
+	<h2>Login</h2>
+	<button on:click={() => signIn('google')}>Google Login</button>
+</section>
