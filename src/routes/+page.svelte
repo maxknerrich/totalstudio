980ee5314618662b5db87a6d14f62aae81a3c061
Max Knerrich <max.knerrich@outlook.com>
2024-08-14 21:58:41 +0200

    home css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,36 +11,66 @@
 	console.log({ files, user });
 </script>
 
-<nav>
-	<h1>Pages</h1>
-	<a href="/new">Create New Page</a>
-</nav>
-<div>
+<header class="container">
+	<h1>Total Studio</h1>
+	<user>
+		<img src={user?.image} alt={user?.name} />
+		<usermeta>
+			<p>{user?.name}</p>
+			<p class="email">{user?.email}</p>
+		</usermeta>
+		<button class="secondary" on:click={() => signOut()}>Sign Out</button>
+	</user>
+</header>
+<section class="container">
+	<nav>
+		<h2>Your Pages</h2>
+		<a href="/new" role="button">+ Create New</a>
+	</nav>
+	<pages>
 	{#each files as file}
 		<a href="/edit/{file}">{file}</a>
 	{/each}
-</div>
-<section>
-	<h1>User</h1>
-	<p>{user?.name}</p>
-	<p>{user?.email}</p>
-	<img src={user?.image} alt={user?.name} />
-	<button on:click={() => signOut()}>Sign Out</button>
+	</pages>
 </section>
 
 <style>
-	section {
-		flex: 0.6;
-		display: flex;
-		padding-top: 4rem;
-		align-items: center;
-		flex-direction: column;
-		justify-content: center;
-	}
-
-	nav {
+	header {
 		display: flex;
 		justify-content: space-between;
 		align-items: baseline;
+		padding-block: 1rem;
+		margin-bottom: 64px;
+	} 
+	user {
+		display: flex;
+		align-items: center;
+	}
+	usermeta {
+		margin-left: 12px;
+		margin-right: 32px;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: center;
+	}
+	usermeta p {
+		margin: 0;
+	}
+	.email {
+		font-size: 0.75rem;
+		opacity: 0.5;
+	}
+	img {
+		border-radius: 50%;
+		width: 48px;
+		height: 48px;
+	}
+	pages a {
+		background-color: rgba(255, 255, 255, .05);
+		display: block;
+		padding: 8px 16px;
+		border-radius: 8px;
+		margin-top: 24px;
 	}
 </style>
